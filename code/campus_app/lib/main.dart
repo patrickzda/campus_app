ec737a1947472ed793043f16909635e798ddbd0e
patrickzda <patrick.zdanowski@gmx.de>
2024-01-09 16:57:37 +0100

    Update main.dart

--- code/campus_app/lib/main.dart
+++ code/campus_app/lib/main.dart
@@ -6,9 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_embed_unity/flutter_embed_unity.dart';
 
 void main(){
-  int counter = 0;
 
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       body: Column(
         children: [
@@ -19,41 +19,41 @@ void main(){
               },
             ),
           ),
-          GestureDetector(
-            onTap: (){
-              //UnityCommunicationService.toggle3dView();
-              List<NavigationNode> route;
-              NavigationService service = NavigationService();
-              Coordinates start = const Coordinates(latitude: 52.51066732441862, longitude: 13.327159187237413);
-              Coordinates end = const Coordinates(latitude: 52.51658837236144, longitude: 13.323546307383321);
-              NavigationNode startNode = service.getClosestNodeToCoordinates(start);
-              NavigationNode endNode = service.getClosestNodeToCoordinates(end);
+          //GestureDetector(
+          //  onTap: (){
+          //    //UnityCommunicationService.toggle3dView();
+          //    List<NavigationNode> route;
+          //    NavigationService service = NavigationService();
+          //    Coordinates start = const Coordinates(latitude: 52.51066732441862, longitude: 13.327159187237413);
+          //    Coordinates end = const Coordinates(latitude: 52.51658837236144, longitude: 13.323546307383321);
+          //    NavigationNode startNode = service.getClosestNodeToCoordinates(start);
+          //    NavigationNode endNode = service.getClosestNodeToCoordinates(end);
 
-              route = service.calculateRoute(startNode, endNode);
-              List<Coordinates> routeCoordinates = List.generate(route.length, (int index){
-                return route[index].coordinates;
-              });
+          //    route = service.calculateRoute(startNode, endNode);
+          //    List<Coordinates> routeCoordinates = List.generate(route.length, (int index){
+          //      return route[index].coordinates;
+          //    });
 
-              UnityCommunicationService.createPolyline(routeCoordinates);
-            },
-            child: Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(
-                vertical: 20
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.blue
-              ),
-              child: const Text(
-                "Display fastest route",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15
-                ),
-              ),
-            ),
-          )
+          //    UnityCommunicationService.createPolyline(routeCoordinates);
+          //  },
+          //  child: Container(
+          //    alignment: Alignment.center,
+          //    width: double.infinity,
+          //    padding: const EdgeInsets.symmetric(
+          //      vertical: 20
+          //    ),
+          //    decoration: const BoxDecoration(
+          //      color: Colors.blue
+          //    ),
+          //    child: const Text(
+          //      "Display fastest route",
+          //      style: TextStyle(
+          //        color: Colors.white,
+          //        fontSize: 15
+          //      ),
+          //    ),
+          //  ),
+          //)
         ],
       ),
     ),
